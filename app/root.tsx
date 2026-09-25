ab155b882d06efadf573f889290f76d0269dd295
Rahul Khimsuriya <rahulkhimsuriya38@gmail.com>
2026-09-26 01:00:25 +0530

    changed favicon

--- app/root.tsx
+++ app/root.tsx
@@ -18,6 +18,8 @@ import { PrivacyModal } from "./components/layout/PrivacyModal";
 import { AppLoader } from "./components/common/AppLoader";
 
 export const links: Route.LinksFunction = () => [
+  { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+  { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
